c0b289efca644330bf1c1eb52029fe89da652370
OzielCortez <116935625+OzielCortez@users.noreply.github.com>
2023-08-24 21:20:51 -0600

    deployServer

--- server/index.js
+++ server/index.js
@@ -1,14 +1,16 @@
 const server = require("./src/app.js");
+require('dotenv').config();
 const { conn } = require("./src/db.js");
 const fillHomes = require("./src/utils/fillHomes.js");
 const fillPets = require("./src/utils/fillPets.js");
+const port = process.env.port || 3000;
 
 // Syncing all the models at once.
 conn.sync({
   force: false}).then(() => {
-  server.listen(3001, () => {
+  server.listen(port, () => {
     fillPets();
     // fillHomes()
-    console.log("%s listening at 3001"); // eslint-disable-line no-console
+    console.log(`%s listening at ${port}`); // eslint-disable-line no-console
   });
 });
