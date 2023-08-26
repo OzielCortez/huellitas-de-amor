7c23a345b1b290ba8f6a05cbf43455ddfb065f02
OzielCortez <116935625+OzielCortez@users.noreply.github.com>
2023-08-25 20:36:05 -0600

    Hola

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  rollupOptions: {
+    external: ['firebase/auth'],
+    // ...otras opciones de Rollup si las tienes
+  },
   plugins: [react()],
 })
